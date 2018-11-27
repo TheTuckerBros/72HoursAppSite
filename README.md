e3198408fff5ce298c907b32274debf0013e0c30
Barlow Tucker <barlowtucker@gmail.com>
2018-11-27 15:42:10 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#72 Hours App
---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var header = require("gulp-header");
+var cleanCSS = require("gulp-clean-css");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var autoprefixer = require("gulp-autoprefixer");
+var pkg = require("./package.json");
+var browserSync = require("browser-sync").create();
+
+// Set the banner content
+var banner = [
+  "/*!\n",
+  " * 72 Hours App",
+  " * Copyright 2018-" + new Date().getFullYear(),
+  " <%= pkg.author %>\n",
+  " */\n",
+  "\n"
+].join("");
+
+// Copy third party libraries from /node_modules into /vendor
+gulp.task("vendor", function(done) {
+  // Bootstrap
+  gulp
+    .src([
+      "./node_modules/bootstrap/dist/**/*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
+    ])
+    .pipe(gulp.dest("./vendor/bootstrap"));
+
+  // Font Awesome 5
+  gulp.src(["./node_modules/@fortawesome/**/*"]).pipe(gulp.dest("./vendor"));
+
+  // jQuery
+  gulp
+    .src([
+      "./node_modules/jquery/dist/*",
+      "!./node_modules/jquery/dist/core.js"
+    ])
+    .pipe(gulp.dest("./vendor/jquery"));
+
+  // jQuery Easing
+  gulp
+    .src(["./node_modules/jquery.easing/*.js"])
+    .pipe(gulp.dest("./vendor/jquery-easing"));
+
+  // Simple Line Icons
+  gulp
+    .src(["./node_modules/simple-line-icons/fonts/**"])
+    .pipe(gulp.dest("./vendor/simple-line-icons/fonts"));
+
+  gulp
+    .src(["./node_modules/simple-line-icons/css/**"])
+    .pipe(gulp.dest("./vendor/simple-line-icons/css"));
+
+  done();
+});
+
+// Compile SCSS
+gulp.task("css:compile", function() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(
+      sass
+        .sync({
+          outputStyle: "expanded"
+        })
+        .on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(
+      header(banner, {
+        pkg: pkg
+      })
+    )
+    .pipe(gulp.dest("./css"));
+});
+
+// Minify CSS
+gulp.task(
+  "css:minify",
+  gulp.series("css:compile", function() {
+    return gulp
+      .src(["./css/*.css", "!./css/*.min.css"])
+      .pipe(cleanCSS())
+      .pipe(
+        rename({
+          suffix: ".min"
+        })
+      )
+      .pipe(gulp.dest("./css"))
+      .pipe(browserSync.stream());
+  })
+);
+
+// CSS
+gulp.task("css", gulp.series("css:compile", "css:minify"));
+
+// Minify JavaScript
+gulp.task("js:minify", function() {
+  return gulp
+    .src(["./js/*.js", "!./js/*.min.js"])
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(
+      header(banner, {
+        pkg: pkg
+      })
+    )
+    .pipe(gulp.dest("./js"))
+    .pipe(browserSync.stream());
+});
+
+// JS
+gulp.task("js", gulp.series("js:minify"));
+
+// Default task
+gulp.task("default", gulp.series("css", "js", "vendor"));
+
+// Configure the browserSync task
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Dev task
+gulp.task(
+  "dev",
+  gulp.series("css", "js", "browserSync", function() {
+    gulp.watch("./scss/*.scss", ["css"]);
+    gulp.watch("./js/*.js", ["js"]);
+    gulp.watch("./*.html", browserSync.reload);
+  })
+);
